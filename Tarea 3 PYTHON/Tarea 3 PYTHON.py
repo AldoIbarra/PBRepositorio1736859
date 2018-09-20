f0491941fa09948afa5f7209e150fc7f49f9855f
Aldo Ibarra <aldomaximino1@hotmail.com>
2018-09-19 20:47:48 -0500

    Tarea 3 en PYTHON agregada al repositorio.

---
+++ Tarea 3 PYTHON/Tarea 3 PYTHON.py
@@ -0,0 +1,5 @@
+print ("suma de los primeros n numeros naturales")
+numero = input('Hasta que numero quieres sumar? ')
+numeroint = int(numero)
+suma = (numeroint * (numeroint + 1))/2
+print("La suma de los n primeros numeros es", suma)
